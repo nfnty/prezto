95f4bdd8a60a4885ae4fcb39916dc97609d46e66
nfnty <git@nfnty.se>
2018-10-15 15:09:26 +0100

    utility: Replace `ls` with `exa`

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -65,10 +65,7 @@ alias type='type -a'
 # ls
 if is-callable 'dircolors'; then
   # GNU Core Utilities
-
-  if zstyle -T ':prezto:module:utility:ls' dirs-first; then
-    alias ls="${aliases[ls]:-ls} --group-directories-first"
-  fi
+  alias ls='exa --group-directories-first'
 
   if zstyle -t ':prezto:module:utility:ls' color; then
     # Call dircolors to define colors if they're missing
@@ -103,17 +100,9 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -a'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # I often screw this up.
+alias l='ls --long --all --group --binary --time=modified --time-style=long-iso --git'
+alias lr='l --recurse'
+alias lt='l --tree'
 
 # Grep
 if zstyle -t ':prezto:module:utility:grep' color; then
